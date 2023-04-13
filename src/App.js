f8dfa5369f14cd1d1ff99bd568c59d2f674060f7
Gufran-Haciakil <gufranakil1@gmail.com>
2023-04-13 20:46:55 +0300

    this my first project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <>
+      <div className="title">
+        Lorem ipsum, dolor sit amet consectetur adipisicing <br /> elit. Id totam
+        ducimus dolorem, quod distinctio, officia enim, <br /> unde reprehenderit amet
+        natus assumenda accusamus non molestias recusandae neque sunt aspernatur
+        dolor ad!
+      </div>
+    </>
+  );
+}
+
+export default App;
